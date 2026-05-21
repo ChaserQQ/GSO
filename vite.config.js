37f1b71c2921a0742b8ced14ed76e2b80e3bfa6e
ChaserQQ <rlaalsdudsk8@gmail.com>
2026-05-21 14:37:46 +0900

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// GitHub Pages base path: must match repo name exactly (case-sensitive)
-// Repo: https://github.com/chaserqq/GSO  ->  base: '/GSO/'
-export default defineConfig({
-  plugins: [react()],
-  base: '/GSO/',
-})
